2daa5a5623783e40465b809950b6f69c863f2581
toot09 <03toot28@gmail.com>
2020-04-17 13:46:15 +0000

    study about basic of GO Language

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
+// compile이 필요한 프로젝트라면 시작 패키지는 main 이름을 가짐
+// package 선언
 package main
 
 import "fmt"
 
+// 제일먼저 실행되는 function이 main function
 func main() {
-  fmt.Println("Hello World")
+  // 대문자로 function을 만들면 다른 프로젝트에서 export 가능
+  //fmt.Print("안녕하세요")
+  
+  //constance(상수) : constance는 변경 불가능
+  const name_c string = "jaewon"
+  fmt.Println(name_c)
+
+  //variable(변수) : variable은 변경 가능 
+  //축약형 :=로 선언가능 ,type 자동매칭 가능 (function 안에서만 가능)
+  //var name_v string = "jaewon"
+  name_v := "ayoung"
+  fmt.Println(name_v)
+  chk := false
+  fmt.Println(chk)
 }
